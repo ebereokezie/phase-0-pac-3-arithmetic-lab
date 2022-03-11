4e27d4ce86693d0e30ee0f6160d33d32d9ea8f25
Ebere Okezie <ebereokezie@gmail.com>
2022-03-10 22:53:01 -0600

    commit tree ce58b0ffb5d00cf1ac3af5b7cc4de85f6697d148

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(){}
+function subtract(){}
+function multiply(){}
+function divide (){}
+function add(x,y){
+    return x+y
+}
+function subtract(x,y){
+    return x-y
+}
+function multiply (x,y){
+    return x*y
+}
+function divide (x,y){
+    return x/y
+}
+function increment(n){
+    return n+=1
+}
+function decrement(n){
+    return n-=1
+}
+function makeInt(n){
+    parseInt(n)
+    return n
+}
+// function makeInt(n){
+//     parseInt("n")
+//     return n
+// }
